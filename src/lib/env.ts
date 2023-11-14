97cd0c8e74c825fb8f31707983bb2f75e6946839
seppa89 <darek0511@gmail.com>
2023-11-14 18:28:30 +0100

    chore: setup db connection

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,7 +1,8 @@
-import { cleanEnv, num } from 'envalid';
+import { cleanEnv, num, str } from 'envalid';
 
 const env = cleanEnv(process.env, {
 	PORT: num(),
+	MONGODB_URI: str(),
 });
 
 export default env;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import 'dotenv/config';
 import env from './lib/env';
 import express from 'express';
+import mongoose from 'mongoose';
+import userRouter from './routes/users';
 
 const PORT = env.PORT;
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.json({ message: 'ok' });
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.use('/api/users', userRouter);
+
+mongoose.connect(env.MONGODB_URI).then(() => {
+	console.log('Connect to database');
+	app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+});
